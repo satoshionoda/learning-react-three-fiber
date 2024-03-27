b4608cf150402594ed1a263aa580634f246c5f24
Satoshi Onoda <satoshionoda@gmail.com>
2024-03-27 16:13:01 +0900

    fix vite config

--- @projects/002/vite.config.ts
+++ @projects/002/vite.config.ts
@@ -4,6 +4,7 @@ import glsl from "vite-plugin-glsl";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "./",
   plugins: [react(), glsl()],
   resolve: {
     alias: [{ find: /^@\//, replacement: `${__dirname}/src/` }],
