114d5020ecdf2625404c24bdb82db6948d3184e5
Satoshi Onoda <satoshionoda@gmail.com>
2024-04-12 18:51:29 +0900

    image optimization

--- @projects/004/vite.config.ts
+++ @projects/004/vite.config.ts
@@ -1,11 +1,12 @@
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import glsl from "vite-plugin-glsl";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   base: "./",
-  plugins: [react(), glsl()],
+  plugins: [react(), glsl(), ViteImageOptimizer({ jpg: { quality: 75 } })],
   resolve: {
     alias: [{ find: /^@\//, replacement: `${__dirname}/src/` }],
   },
